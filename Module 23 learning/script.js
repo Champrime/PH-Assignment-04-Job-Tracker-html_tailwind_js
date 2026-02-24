f3e90e8b043091c45f25762c9a36532220447833
null <null>
2026-02-24 14:59:57 +0600

    Midway Learning Commit

---
+++ Module 23 learning/script.js
@@ -0,0 +1,22 @@
+let makeYellowV = document.querySelector(".make-yellow");
+console.log(makeYellowV);
+
+let makeYellow = () => {
+  makeYellowV.style.backgroundColor = "#FFFF00";
+};
+
+//-----------------------------------------------
+
+console.log(makeYellow);
+
+const makeOrange = document.querySelector(".make-orange");
+
+makeOrange.onclick = () => {
+  makeOrange.style.backgroundColor = "orange";
+};
+
+//--------------------------------------------------
+
+const makePurple = document.querySelector(".make-purple")
+
+makePurple.addEventListener() = () => {}
--- src/script.js
+++ src/script.js
@@ -1,5 +1,21 @@
-const x = document.querySelectorAll(".menu-selector");
+const menuSelector = document.querySelectorAll(".menu-selector");
+const interviewClickSelector = document.querySelector(".interview-click");
+const interviewCount = document.getElementById("interview-count")
+const rejectCLickSelector = document.querySelector(".reject-click");
 
-const interviewClick = () => {
-    document.getElementById("interviewCount").innerHTML = Number(document.getElementById("interviewCount").innerText) + 1;
-} 
+
+interviewClickSelector.onclick = () => {
+    while (interviewCount.innerText < 1) {
+        interviewCount.innerText = Number(interviewCount.innerText) + 1;
+        if (interviewCount.innerText == 1) {interviewClickSelector.disabled = true;}
+        // if (rejectCLick()){document.getElementById("interviewCount").innerHTML = Number(document.getElementById("interviewCount").innerText) - 1}
+    }
+}
+
+// const jobCards =  document.querySelectorAll(".Job-Card");
+// const interview = document.getElementById("interviewCount");
+// const jobArray = [];
+// for (let job of jobCards){
+//     jobArray.push(job);
+// }
+
