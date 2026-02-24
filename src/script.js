deced3a2cdaafef6797683d5cac8371d59564977
Shakawat Hossain Sadik <saadiq9x@gmail.com>
2026-02-24 23:38:26 +0600

    Commit for Submission

--- src/script.js
+++ src/script.js
@@ -1,15 +1,29 @@
 const menuSelector = document.getElementsByClassName("menu-selector");
+const interviewCount = document.getElementById("interview-count");
+const rejectCount = document.getElementById("reject-count");
 const interviewClick1 = document.getElementById("interview-click-1");
-const interviewCount = document.getElementById("interview-count")
 const rejectClick1 = document.getElementById("reject-click-1");
 
 console.log(interviewClick1);
 interviewClick1.addEventListener ("click", () => {
-    interviewCount.innerText = Number(interviewCount.innerText) + 1;
-    // if (rejectCLick()){document.getElementById("interviewCount").innerHTML = Number(document.getElementById("interviewCount").innerText) - 1}
-    interviewClick1.style.opacity = "50";
-    document.getElementById("not-applied-1").innerText = "Applied";
+    if (interviewCount.innerText < 1){
+        interviewCount.innerText = Number(interviewCount.innerText) + 1;
+        // if (rejectCLick()){document.getElementById("interviewCount").innerHTML = Number(document.getElementById("interviewCount").innerText) - 1}
+        interviewClick1.style.opacity = "50%";
+        rejectClick1.style.opacity = "100%";
+        document.getElementById("not-applied-1").innerText = "Applied";
+        document.createElement()
+    }
+}, {once: true})
 
+rejectClick1.addEventListener ("click", () => {
+    if (rejectCount.innerText < 1){
+        rejectCount.innerText = Number(rejectCount.innerText) + 1;
+        // if (rejectCLick()){document.getElementById("interviewCount").innerHTML = Number(document.getElementById("interviewCount").innerText) - 1}
+        rejectClick1.style.opacity = "50%";
+        interviewClick1.style.opacity = "100%";
+        document.getElementById("not-applied-1").innerText = "Not Interested";
+    }
 }, {once: true})
 
 // const jobCards =  document.querySelectorAll(".Job-Card");
